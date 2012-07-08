dc79ae34a33674d5aee473a55f6edae214639292
caili <ca168168@gmail.com>
2012-07-08 22:51:25 +0200

    yaml, rspec

--- emacs.d/cl.el
+++ emacs.d/cl.el
@@ -3,14 +3,33 @@
 (when (getenv "TERM")
   (remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode))
 
-(delete-selection-mode t)
-
-(setq-default save-place t)
-
-(setq-default cursor-type 'bar)
+(if (fboundp 'menu-bar-mode) (menu-bar-mode 1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode 1))
 
 (recentf-mode 1)
 (global-set-key "\C-xf" 'recentf-open-files)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(delete-selection-mode t)
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(setq-default save-place t)
+(setq-default cursor-type 'bar)
+
+;http://stackoverflow.com/questions/69934/set-4-space-indent-in-emacs-in-text-mode
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
+(setq stack-trace-on-error t)
+;(require 'ecb)
+
+(set-variable 'ffip-limit 2048)
+(set-variable 'ffip-patterns '("*.html" "*.css" "*.rb" "*.yml" "*.feature" "*.js" "*.erb" "*.haml" ))
+(set-variable 'ffip-find-options "-not -regex \".*vendor.*\" -not -regex \".*node_modules.*\"")
 
 
 (add-hook 'html-mode-hook '(lambda () (auto-fill-mode 0)))
@@ -23,39 +42,6 @@
 ;(add-hook 'ruby-mode-hook '(lambda ()
 ;                             (local-set-key (kbd "RET") 'newline-and-indent)))
 
-
-(if (fboundp 'menu-bar-mode) (menu-bar-mode 1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode 1))
-
-
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;(global-linum-mode 1)
-
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-
-
-                                        ;
-;http://stackoverflow.com/questions/69934/set-4-space-indent-in-emacs-in-text-mode
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq indent-line-function 'insert-tab)
-
-
-
-;;(require 'color-theme)
-;;(setq color-theme-is-global t)
-;;;(color-theme-hober)
-
-
-
-;(add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/ecb"))
-(setq stack-trace-on-error t)
-;(require 'ecb)
-
-
-(set-variable 'ffip-limit 2048)
-(set-variable 'ffip-patterns '("*.html" "*.css" "*.rb" "*.yml" "*.feature" "*.js" "*.erb" "*.haml" ))
-(set-variable 'ffip-find-options "-not -regex \".*vendor.*\" -not -regex \".*node_modules.*\"")
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
